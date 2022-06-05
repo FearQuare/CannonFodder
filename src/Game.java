110cff5f411fb68e574db9cde877c0159382b073
Elmira <elmira.aras@protonmail.com>
2022-06-05 19:46:39 +0300

    Update 5

--- src/Game.java
+++ src/Game.java
@@ -87,7 +87,26 @@ public class Game {
     }
 
     public Weapons dropWeapon(){
-        Weapons[] drop = new Weapons[10];
+        Weapons[] drop = new Weapons[15];
+        //Swords
+        drop[0] = new Swords("Storm-Weaver",1,1,1);
+        drop[1] = new Swords("Destiny's Song",2,2,3);
+        drop[2] = new Swords("Gutrender",2,4,5);
+        drop[3] = new Swords("Terror Deflector",3,1,2);
+        drop[4] = new Swords("Hope's Swiftblade",1,5,4);
+        //Shields
+        drop[5] = new Shields("The Void", 1,1,1);
+        drop[6] = new Shields("Malice",1,3,2);
+        drop[7] = new Shields("Overture",2,5,5);
+        drop[8] = new Shields("Whistling Barrier",2,1,1);
+        drop[9] = new Shields("Storm-Forged Buffer",3,1,1);
+        //Wands
+        drop[10] = new Wands("Will of the Master",1,1,1);
+        drop[11] = new Wands("Stormbringer",1,3,4);
+        drop[12] = new Wands("Pansophy",1,2,2);
+        drop[13] = new Wands("Blood Infused Branch",3,3,4);
+        drop[14] = new Wands("Frozen Spire",1,2,1);
+
         int index  = rand.nextInt(0,9);
         return drop[index];
     }

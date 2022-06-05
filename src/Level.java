7f71a8bb2e9c592f9b501dbf11f6fae091dd41e8
Elmira <elmira.aras@protonmail.com>
2022-06-05 20:23:56 +0300

    Update 5

--- src/Level.java
+++ src/Level.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+
 interface Level {
-    static int level = 0;
-    static double enemyAmount = 0;
-    static int turn = 0;
+    public void increaseLevel();
+    public static int turnCountChar(){
+        int turn = 0;
+        if(turn<2){
+            turn ++;
+            return turn;
+        }else {
+            turn = 0;
+            return turn;
+        }
+    }
+    public static int turnCountEnemy(int turnEn, ArrayList<EnemySoldier> enemies){
+        int turn = 0;
+        if(turn<enemies.size()){
+            turn++;
+            return turn;
+        }else{
+            turn = 0;
+            return turn;
+        }
+    }
+    public void setEnemy();
+    public Weapons dropWeapon();
+    public void attack();
+    public void specialAction();
+    public void pick();
+    public void wield();
+    public void wear();
+    public void examine();
+    public void listInventory();
 
 }

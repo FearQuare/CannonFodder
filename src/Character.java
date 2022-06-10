d8db1fe4973fe5744ceaa46c1b8b647a84663547
Elmira <elmira.aras@protonmail.com>
2022-06-10 17:45:10 +0300

    IDK which update this is anymore

--- src/Character.java
+++ src/Character.java
@@ -256,6 +256,7 @@ public abstract class Character implements ICharacterMethods{
             case 1:
                 //Adding wielded weapon to your inventory
                 inventoryW.add(wieldedWeapon);
+                updateInventory();
                 System.out.println(wieldedWeapon.name + " has been added to your inventory.");
                 wieldedWeapon = null; //Empty hand
 
@@ -280,6 +281,7 @@ public abstract class Character implements ICharacterMethods{
                         }else{
                             wieldedWeapon = (Weapons) inventoryW.get(wield);
                             inventoryW.remove(wield);
+                            updateInventory();
                         }
                         break;
                     default:
@@ -288,6 +290,7 @@ public abstract class Character implements ICharacterMethods{
             case 2:
                 //Adding wielded clothing to your inventory
                 inventoryC.add(wieldedClothing);
+                updateInventory();
                 System.out.println(wieldedClothing.name + " has been added to your inventory.");
                 wieldedClothing = null; //Empty hand
 
@@ -312,6 +315,7 @@ public abstract class Character implements ICharacterMethods{
                         }else{
                             wieldedClothing = (Clothings) inventoryC.get(wield1);
                             inventoryC.remove(wield1);
+                            updateInventory();
                         }
                         break;
                     default:
